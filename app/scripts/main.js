e3bc3582c6e59e31a4f6025125d2b44b757dd687
filigreti <aolfiligre@gmail.com>
2020-05-29 23:43:07 +0100

    finished test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -79,9 +79,139 @@
       });
   }
 })();
+
 let baconImage = document.getElementById("bacon-image");
 let baconImageContainer = document.getElementById("bacon-image-container");
+let form = document.getElementById("form");
+let firstName = document.getElementById("user-firstname");
+let lastName = document.getElementById("user-lastname");
+let email = document.getElementById("user-email");
+let post = document.getElementById("user-post");
+let phone = document.getElementById("user-phone");
+let card = document.getElementById("user-card");
+let code = document.getElementById("user-code");
+let date = document.getElementById("user-date");
+
+document.getElementById("user-card").addEventListener("input", function (e) {
+  let foo = this.value.split("-").join("");
+  if (foo.length > 0) {
+    foo = foo.match(new RegExp(".{1,4}", "g")).join("-");
+  }
+  this.value = foo;
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validate();
+});
+
+function validate() {
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const postValue = post.value.trim();
+  const phoneValue = phone.value.trim();
+  const cardValue = card.value.trim();
+  const codeValue = code.value.trim();
+  const dateValue = date.value.trim();
+
+  if (firstNameValue === "" || firstNameValue === null) {
+    setError(firstName, "First name must not be empty");
+  } else {
+    setSuccess(firstName);
+  }
+
+  if (lastNameValue === "" || lastNameValue === null) {
+    setError(lastName, "Last name must not be empty");
+  } else {
+    setSuccess(lastName);
+  }
+
+  if (postValue === "" || postValue === null) {
+    setError(post, "Postal Code must not be empty");
+  } else if (postValue.length <= 2) {
+    setError(post, "Enter a valid post");
+  } else {
+    setSuccess(post);
+  }
+
+  if (phoneValue === "" || phoneValue === null) {
+    setError(phone, "Phone Number must not be empty");
+  } else if (phoneValue.length < 5) {
+    setError(phone, "Phone Number can not be less than 5");
+  } else {
+    setSuccess(phone);
+  }
+
+  if (cardValue === "" || cardValue === null) {
+    setError(card, "Credit Card  must not be empty");
+  } else if (cardValue.length < 12) {
+    setError(card, "Credit Card Number can not be less than 12");
+  } else {
+    setSuccess(card);
+  }
+
+  if (codeValue === "" || codeValue === null) {
+    setError(code, "Pin must not be empty");
+  } else if (codeValue.length < 3) {
+    setError(code, "Pin can not be less than 3");
+  } else {
+    setSuccess(code);
+  }
+
+  if (dateValue === "" || dateValue === null) {
+    setError(date, "Date must not be empty");
+  } else {
+    setSuccess(date);
+  }
+
+  if (emailValue === "" || emailValue === null) {
+    setError(email, "Email cannot be empty");
+  } else if (!isEmail(emailValue)) {
+    setError(email, "Not a valid email");
+  } else {
+    setSuccess(email);
+  }
+
+  if (
+    firstNameValue &&
+    lastNameValue &&
+    emailValue &&
+    phoneValue &&
+    postValue &&
+    cardValue &&
+    codeValue &&
+    dateValue
+  ) {
+    alert("Form Submitted Successfully");
+  }
+}
+
 function cloneBaconImage() {
   let clone = baconImage.cloneNode(true);
   baconImageContainer.appendChild(clone);
+}
+
+function setError(input, message) {
+  const inputContainer = input.parentElement;
+  const small = inputContainer.querySelector("small");
+  inputContainer.className = "input-container error";
+  small.innerText = message;
+}
+
+function setSuccess(input) {
+  console.log(input.id);
+  if (input.id == "user-card") {
+    const inputContainer = input.parentElement;
+    inputContainer.className = "overall";
+  } else {
+    const inputContainer = input.parentElement;
+    inputContainer.className = "input-container";
+  }
+}
+
+function isEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
